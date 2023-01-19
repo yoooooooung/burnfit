feb91af3acec7359d51b3bd8e2ed5b40ed5a3d13
yoooooooung <oaoa9n@gmail.com>
2023-01-20 00:58:03 +0900

    Level 1 : Bottom Tabs Navigator Icon 변경 및 StatusBar 추가

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { View, Text } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import { StatusBar } from "expo-status-bar";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Icon2 from "react-native-vector-icons/Ionicons";
 
 const HomeScreen = () => {
   return (
@@ -40,14 +42,49 @@ const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="HOME" component={HomeScreen} />
-        <Tab.Screen name="CALENDAR" component={CalendarScreen} />
-        <Tab.Screen name="LIBRARY" component={LibraryScreen} />
-        <Tab.Screen name="MY PAGE" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="HOME"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="CALENDAR"
+            component={CalendarScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="calendar-month-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="LIBRARY"
+            component={LibraryScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon2 name="library-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MY PAGE"
+            component={ProfileScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon2 name="person-outline" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </>
   );
 };
 
