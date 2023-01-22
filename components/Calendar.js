10526e93136d79d65927a8e7ce06e5ddfbdbd0a0
yoooooooung <oaoa9n@gmail.com>
2023-01-22 23:54:01 +0900

    Level 2 : Calendar - 오늘날짜 표시

--- components/Calendar.js
+++ components/Calendar.js
@@ -22,13 +22,16 @@ const Calendar = () => {
 
   const monthNumber = new Date().getMonth(); //month 숫자로
   const year = new Date().getFullYear();
+  const date = new Date().getDate();
 
   const [selectMonth, setSelectMonth] = useState(monthNumber);
   const [selectYear, setSelectYear] = useState(year);
 
-  const month = monthList[selectMonth]; //month 영어로
+  //기능3 구현중
+  const today = String(year) + String(monthNumber + 1) + String(date);
+  const [clickDay, setClickDay] = useState(today);
 
-  //const date = new Date().getDate();
+  const month = monthList[selectMonth]; //month 영어로
 
   //이번달 마지막 날
   const lastDate = new Date(selectYear, selectMonth + 1, 0).getDate();
@@ -49,10 +52,6 @@ const Calendar = () => {
   //다음달 날짜 필요한 갯수 : 35 - (저번달 + 이번달 날짜 갯수)
   const nextMonthList = Array.from({ length: 6 - lastDay }, (v, i) => i + 1);
 
-  //달력에 뿌려줄 날들
-  const dateList = [...lastMonthList, ...thisMonthList, ...nextMonthList];
-  console.log(dateList);
-
   const toPrevMonth = () => {
     if (selectMonth == 0) {
       setSelectYear((a) => a - 1);
@@ -69,6 +68,8 @@ const Calendar = () => {
       setSelectMonth((a) => a + 1);
     }
   };
+
+  console.log(year === selectYear && selectMonth === monthNumber);
 
   return (
     <View style={styles.makeCenter}>
@@ -101,31 +102,22 @@ const Calendar = () => {
           <Text style={styles.notThisMonth}>{b}</Text>
         ))}
         {thisMonthList.map((b) => (
-          <Text style={styles.thisMonth}>{b}</Text>
+          <Text
+            style={[
+              styles.thisMonth,
+              year === selectYear &&
+                selectMonth === monthNumber &&
+                b === date &&
+                styles.clicked,
+            ]}
+          >
+            {b}
+          </Text>
         ))}
         {nextMonthList.map((b) => (
           <Text style={styles.notThisMonth}>{b}</Text>
         ))}
       </View>
-
-      {/* <View>
-        <Text>이번달은 며칠까지 있을까 : {lastDate}</Text>
-        <Text>이번달은 무슨요일부터? : {firstDay}</Text>
-        <Text>thisMonthList :</Text>
-        {thisMonthList.map((b) => (
-          <Text>{b}</Text>
-        ))}
-
-        <Text>lastMonthList :</Text>
-        {lastMonthList.map((b) => (
-          <Text>{b}</Text>
-        ))}
-
-        <Text>nextMonthList :</Text>
-        {nextMonthList.map((b) => (
-          <Text>{b}</Text>
-        ))}
-      </View> */}
     </View>
   );
 };
@@ -157,13 +149,22 @@ const styles = StyleSheet.create({
 
   thisMonth: {
     width: "14.2%",
-    paddingTop: 30,
+    paddingTop: 15,
+    paddingBottom: 15,
     textAlign: "center",
   },
+
   notThisMonth: {
     color: "gray",
     width: "14.2%",
-    paddingTop: 30,
+    paddingTop: 15,
+    paddingBottom: 15,
     textAlign: "center",
+  },
+
+  clicked: {
+    borderColor: "skyblue",
+    borderWidth: 1,
+    borderRadius: "25%",
   },
 });
