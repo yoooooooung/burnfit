7b9b7cb1db44d6a794d2de57bbaa322f2a233f1a
yoooooooung <oaoa9n@gmail.com>
2023-01-22 21:55:53 +0900

    Level 2 : Calendar - Header 완성

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
@@ -7,20 +8,65 @@ const Calendar = () => {
   //     timezone: "asia/korea",
   //   });
   //   const dateNormal = new Date().toString();
+  const monthList = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "April",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const weekList = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
   const date = new Date().getDate();
-  const month = new Date().toString().split(" ")[1];
+  //   const month = new Date().toString().split(" ")[1];
+  const monthNumber = new Date().getMonth();
   const year = new Date().getFullYear();
+
+  const [selectMonth, setSelectMonth] = useState(monthNumber);
+  const [selectYear, setSelectYear] = useState(year);
+
+  const month = monthList[selectMonth];
+
+  const toPrevMonth = () => {
+    if (selectMonth == 0) {
+      setSelectYear((a) => a - 1);
+      setSelectMonth(11);
+    } else {
+      setSelectMonth((a) => a - 1);
+    }
+  };
+  const toNextMonth = () => {
+    if (selectMonth == 11) {
+      setSelectYear((a) => a + 1);
+      setSelectMonth(0);
+    } else {
+      setSelectMonth((a) => a + 1);
+    }
+  };
 
   return (
     <View style={{ flex: 1, flexDirection: "column", alignItems: "center" }}>
       <View style={styles.header}>
-        <Pressable style={{ flex: 1, alignItems: "center" }}>
+        <Pressable
+          style={{ flex: 1, alignItems: "center" }}
+          onPress={toPrevMonth}
+        >
           <Icon name="chevron-left" color="skyblue" size={30} />
         </Pressable>
         <Text style={{ flex: 3, fontSize: 22, textAlign: "center" }}>
-          {month} {year}
+          {month} {selectYear}
         </Text>
-        <Pressable style={{ flex: 1, alignItems: "center" }}>
+        <Pressable
+          style={{ flex: 1, alignItems: "center" }}
+          onPress={toNextMonth}
+        >
           <Icon name="chevron-right" color="skyblue" size={30} />
         </Pressable>
       </View>
